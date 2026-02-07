087968404211adce7bf4d78307d114c913113391
Juhyeong Pang <juhyeong13579@gmail.com>
2026-02-06 23:45:16 -0600

    MLM Prepare

---
+++ config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    MAX_LEN = 256
+    BATCH_SIZE = 32
+    LR = 0.001
+    VOCAB_SIZE = 30000
+    EMBED_DIM = 128
+    NUM_HEAD = 8
+    FF_DIM = 512
+    NUM_LAYERS = 4
+
+config = Config()
